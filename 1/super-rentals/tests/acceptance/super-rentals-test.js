048eecdc13820a50e2b81918a1e35c10d3159036
Keian Rao <keian.rao@gmail.com>
2021-05-28 21:07:08 -0400

    Added acceptance test for nav bar.

--- 1/super-rentals/tests/acceptance/super-rentals-test.js
+++ 1/super-rentals/tests/acceptance/super-rentals-test.js
@@ -8,9 +8,15 @@ module('Acceptance | super rentals', function(hooks) {
 	test('visiting /super-rentals', async function(assert) {
 		await visit('/');
 		assert.equal(currentURL(), '/');
+		
+		assert.dom('nav');
+		assert.dom('h1').hasText('SuperRentals');
+		// Not even limiting it to a child of nav?
+		
 		assert.dom('h2').hasText('Welcome to Super Rentals!');
 		assert.dom('.jumbo a.button').hasText('About Us');
 		// assert.dom will assert fail if element not found.
+		
 		await click('.jumbo a.button');
 		assert.equal(currentURL(), '/about');
 	
@@ -20,8 +26,13 @@ module('Acceptance | super rentals', function(hooks) {
 	test('visiting /about', async function (assert) {
 		await visit('/about');
 		assert.equal(currentURL(), '/about');
+		
+		assert.dom('nav');
+		assert.dom('h1').hasText('SuperRentals');
+		
 		assert.dom('h2').hasText('About Super Rentals');
 		assert.dom('.jumbo a.button').hasText('Contact Us');
+		
 		await click('.jumbo a.button');
 		// I should have noticed before that, click also targets by
 		// CSS selector, rather than asking to be passed an object?
@@ -31,12 +42,40 @@ module('Acceptance | super rentals', function(hooks) {
   	test('visiting /getting-in-touch', async function (assert) {
 		await visit('/getting-in-touch');		
 		assert.equal(currentURL(), '/getting-in-touch');
+		
+		assert.dom('nav');
+		assert.dom('h1').hasText('SuperRentals');
+		
 		assert.dom('h2').hasText('Contact Us');
 		assert.dom('.jumbo a.button').hasText('About Us');
+		
 		await click('.jumbo a.button');
 		assert.equal(currentURL(), '/about');
 	});
 	
 	// And are we to test the rental entries later, under this style?
 	// These are presence checks of static elements.
+	
+	test('navigating using the nav-bar', async function (assert) {
+		await visit('/');
+		
+		assert.dom('nav');
+		assert.dom('nav a.menu-index').hasText('SuperRentals');
+		assert.dom('nav a.menu-about').hasText('About');
+		assert.dom('nav a.menu-contact').hasText('Contact');
+		// Truly, why are we checking strings when they are subject to
+		// frequent change and localisation..? We should just be checking
+		// for the existence and functions of those links.
+		
+		await click('nav a.menu-about');
+		assert.equal(currentURL(), '/about');
+		
+		await click('nav a.menu-contact');
+		assert.equal(currentURL(), '/getting-in-touch');
+		// Can we also introspect what route we are in?
+		// So that we do not depend on a URL.
+		
+		await click('nav a.menu-index');
+		assert.equal(currentURL(), '/');
+	});
 });
