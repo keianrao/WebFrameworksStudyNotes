d250ced873cdcad31825633606b5833a35a5eff1
Keian Rao <keian.rao@gmail.com>
2021-06-25 15:49:36 -0400

    Added route model in Ember.

---
+++ 1/super-rentals/app/routes/index.js
@@ -0,0 +1,24 @@
+import Route from '@ember/routing/route';
+
+export default class IndexRoute extends Route {
+	async model() {
+		return {
+			'title': 'Grand Old Mansion',
+			'owner': 'Veruca Salt',
+			'city': 'San Francisco',
+			'location': {
+				lat: 37.7749,
+				lng: -122.4194
+			},
+			'category': 'Estate',
+			'type': 'Standalone',
+			'bedrooms': 15,
+			'image': 'assets/images/Crane_estate_(5).jpg',
+			'description':
+				'This grand old mansion sits on over 100 acres'
+				+ ' of rolling hills and dense redwood forests.'
+		};
+		// Returning a constant model for now.
+		// Note that this is not an array.
+	}
+}
